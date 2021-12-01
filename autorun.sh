202e504c9eda8b3b2e23b9b4483fa3d5615ceabb
hoekl <85690629+hoekl@users.noreply.github.com>
2021-12-01 15:36:31 +0000

    Create adduser functionality

--- autorun.sh
+++ autorun.sh
@@ -8,3 +8,24 @@ echo -e "APT::Periodic::Update-Package-Lists \"1\";\nAPT::Periodic::Unattended-U
 sed -i 's|//Unattended-Upgrade::Remove-Unused-Dependencies "false"|Unattended-Upgrade::Remove-Unused-Dependencies "true"|' /etc/apt/apt.conf.d/50unattended-upgrades
 printf "\n\nAdding new LUKS key...\n"
 cryptsetup -y luksAddKey -S 1 /dev/nvme0n1p3
+printf "Enter UoB username of new user: "
+read username
+printf "Enter full name of user: "
+read fullname
+adduser --gecos "" --disabled-password $username
+usermod -c "$fullname" $username
+check=0
+while [ $check = 0 ];
+do
+  printf "Enter new password: "
+  read pass1
+  printf "Verify password: "
+  read pass2
+  if [ "$pass1" = "$pass2" ]
+  then
+  check=1
+  fi
+done
+echo "$username:$pass1" | chpasswd
+usermod -a -G $username
+
